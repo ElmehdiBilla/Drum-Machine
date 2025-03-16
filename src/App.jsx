7ba3f65d15771c7a99e7a8815495d4e1fe825546
elmehdibilla <billamehdi13@gmail.com>
2025-03-16 01:02:50 +0000

    add the drum components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import PadBank from "./components/PadBank"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [volume, setVolume] = useState(50);
+  const [kitSwitch, setKitSwitch] = useState(false);
+  const [display, setDisplay] = useState('');
+
+  const handleChange = (e) => {
+    setVolume(e.target.value);
+  }
+  const handleCheck = () => {
+    setKitSwitch(prevS => !prevS);
+  }
+  const changeDisplay = (text) => {
+    setDisplay(text)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="drum-machine">
+        <PadBank kitSwitch={kitSwitch} vol={volume} changeDisplay={changeDisplay}/>
+        <div id="controls">
+          <div id="volume">
+            <label>Vol: {volume}</label>
+            <input type="range" min='1' max='100' value={volume} onChange={handleChange}/>
+          </div>
+          <div id="kit-switch">
+            <label>{!kitSwitch ? 'Heater Kit' : 'Smooth Piano Kit'}</label>
+            <label className="switch">
+              <input type="checkbox" checked={kitSwitch} onChange={handleCheck}/>
+            </label>
+          </div>
+          <div id="display">{display}</div>
+        </div>
+    </div>
   )
 }
 
